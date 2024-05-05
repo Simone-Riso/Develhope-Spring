0807ad5e434ccb891b8b56d752382d4093446399
Simone <ez.simone.riso@gmail.com>
2024-05-05 15:34:57 +0200

    Esercizio666

---
+++ ControlleConPOST/src/main/java/com/example/ControlleConPOST/Esercizio666Application.java
@@ -0,0 +1,13 @@
+package com.example.ControlleConPOST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio666Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio666Application.class, args);
+	}
+
+}
---
+++ ControlleConPOST/src/test/java/com/example/ControlleConPOST/Esercizio666ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ControlleConPOST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio666ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
