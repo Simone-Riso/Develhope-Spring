fd01be9a9087864455e39f8a2b4f63bb6ac9b4e0
Simone Riso <ez.simone.riso@gmail.com>
2024-05-08 12:21:35 +0200

    Create NameController.java

---
+++ esercizio_Controller_Con_GET_e_POST/src/main/java/com/example/ControllerConGETePOST/Controller/NameController.java
@@ -0,0 +1,18 @@
+package com.example.ControllerConGETePOST.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class NameController {
+
+    @GetMapping("/name")
+    public String getName(@RequestParam String name) {
+        return name;
+    }
+
+    @PostMapping("/reverseName")
+    public String reverseName(@RequestParam String name) {
+        return new StringBuilder(name).reverse().toString();
+    }
+
+}
