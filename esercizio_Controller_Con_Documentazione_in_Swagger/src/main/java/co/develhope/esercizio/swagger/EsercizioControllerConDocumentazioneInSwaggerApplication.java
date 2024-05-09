2d8f49446418f6015c31cb357f68a2c0bb33522e
Simone Riso <ez.simone.riso@gmail.com>
2024-05-09 22:32:30 +0200

    Esercizio swagger

---
+++ esercizio_Controller_Con_Documentazione_in_Swagger/src/main/java/co/develhope/esercizio/swagger/EsercizioControllerConDocumentazioneInSwaggerApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizio.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioControllerConDocumentazioneInSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioControllerConDocumentazioneInSwaggerApplication.class, args);
+	}
+
+}
