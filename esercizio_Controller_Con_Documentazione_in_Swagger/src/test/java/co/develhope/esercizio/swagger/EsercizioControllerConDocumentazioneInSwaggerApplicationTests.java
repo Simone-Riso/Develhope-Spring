ee6696bf3e56840bb8e3e521f57e8f28b88a1e21
Simone Riso <ez.simone.riso@gmail.com>
2024-05-10 19:20:26 +0200

    Esercizio swagger

---
+++ esercizio_Controller_Con_Documentazione_in_Swagger/src/test/java/co/develhope/esercizio/swagger/EsercizioControllerConDocumentazioneInSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizio.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioControllerConDocumentazioneInSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
