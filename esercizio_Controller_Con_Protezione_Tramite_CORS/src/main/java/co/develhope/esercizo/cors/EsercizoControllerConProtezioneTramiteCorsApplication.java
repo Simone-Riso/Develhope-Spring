5943580d0c3d173bbe1908c60f78f89fb18102e1
Simone Riso <ez.simone.riso@gmail.com>
2024-05-11 19:58:09 +0200

    Esercizio CORS

---
+++ esercizio_Controller_Con_Protezione_Tramite_CORS/src/main/java/co/develhope/esercizo/cors/EsercizoControllerConProtezioneTramiteCorsApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizo.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizoControllerConProtezioneTramiteCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizoControllerConProtezioneTramiteCorsApplication.class, args);
+	}
+
+}
---
+++ esercizio_Controller_Con_Protezione_Tramite_CORS/src/test/java/co/develhope/esercizo/cors/EsercizoControllerConProtezioneTramiteCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizo.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizoControllerConProtezioneTramiteCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
