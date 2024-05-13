c8452f620d7a9d54f6f2faa79a20518f1213bdce
Simone Riso <ez.simone.riso@gmail.com>
2024-05-13 22:02:50 +0200

    Update NameController.java

--- esercizio_Controller_Con_Documentazione_in_Swagger/src/main/java/co/develhope/esercizio/swagger/controller/NameController.java
+++ esercizio_Controller_Con_Documentazione_in_Swagger/src/main/java/co/develhope/esercizio/swagger/controller/NameController.java
@@ -1,17 +1,26 @@
 package co.develhope.esercizio.swagger.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class NameController {
 
-    @GetMapping("/name")
-    public String getName(@RequestParam String name) {
+    @Operation(
+            summary = "Per ritornare il nome inserito",
+            description = "Una volta inserito un qualsiasi nome, questo verra restituito")
+    @GetMapping(value = "/name", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getName(@Parameter(description = "Nome che si vuole ritornare") @RequestParam String name) {
         return name;
     }
 
-    @PostMapping("/reverseName")
-    public String reverseName(@RequestParam String name) {
+    @Operation(
+            summary = "Per invertire il nome inserito",
+            description = "Una volta inserito un nome lo si riceve al contrario grazie al metodo StringBuilder")
+    @PostMapping(value = "/reverseName", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String reverseName(@Parameter(description = "Nome da invertire") @RequestParam String name) {
         return new StringBuilder(name).reverse().toString();
     }
 
