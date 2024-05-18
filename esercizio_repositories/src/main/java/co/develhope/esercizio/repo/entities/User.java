9764a679aa920e02b66679c9c0f2b48e101fe925
Simone Riso <ez.simone.riso@gmail.com>
2024-05-18 15:19:51 +0200

    Update files

---
+++ esercizio_repositories/src/main/java/co/develhope/esercizio/repo/entities/User.java
@@ -0,0 +1,33 @@
+package co.develhope.esercizio.repo.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private LocalDate birthDate;
+
+    private String city;
+
+}
