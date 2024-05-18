01ebd4f5b3ad88afa8c71ad1105e6008dd1df0db
Simone Riso <ez.simone.riso@gmail.com>
2024-05-18 14:57:24 +0200

    Create files

---
+++ esercizio_repositories/src/main/java/co/develhope/esercizio/repo/EsercizioRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizio.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioRepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioRepositoriesApplication.class, args);
+	}
+
+}
---
+++ esercizio_repositories/src/test/java/co/develhope/esercizio/repo/EsercizioRepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizio.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioRepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
