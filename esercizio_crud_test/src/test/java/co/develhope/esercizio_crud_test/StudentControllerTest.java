359bd51a6bb35323ffbc1064a9f3236d61a4ed52
Simone Riso <ez.simone.riso@gmail.com>
2024-05-24 22:47:09 +0200

    Update esercizio

--- esercizio_crud_test/src/test/java/co/develhope/esercizio_crud_test/StudentControllerTest.java
+++ esercizio_crud_test/src/test/java/co/develhope/esercizio_crud_test/StudentControllerTest.java
@@ -1,43 +1,135 @@
 package co.develhope.esercizio_crud_test;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import co.develhope.esercizio_crud_test.controllers.StudentController;
 import co.develhope.esercizio_crud_test.entities.Student;
 import co.develhope.esercizio_crud_test.service.StudentService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StudentControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private StudentService studentService;
 
     @Test
+    @Order(1)
     public void testCreateStudent() throws Exception {
         Student student = new Student();
-        student.setName("Simon");
+        student.setId(1L);
+        student.setName("Simone");
+        student.setLastName("Riso");
         student.setWorking(true);
 
         when(studentService.createStudent(any(Student.class))).thenReturn(student);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/students")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"name\": \"Simon\", \"working\": true }")
+                        .content("{ \"id\": 1, \"firstName\": \"Simone\", \"lastName\": \"Riso\", \"working\": true }")
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Simon"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.working").value(true));
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Simone"))
+                .andExpect(jsonPath("$.lastName").value("Riso"))
+                .andExpect(jsonPath("$.working").value(true));
     }
+
+
+    @Test
+    @Order(2)
+    public void testGetAllStudents() throws Exception {
+        when(studentService.getAllStudents()).thenReturn(Collections.singletonList(new Student(
+                1L,
+                "Simone",
+                "Riso",
+                true)));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/students"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].name").value("Simone"))
+                .andExpect(jsonPath("$[0].lastName").value("Riso"))
+                .andExpect(jsonPath("$[0].working").value(true));
+    }
+
+    @Test
+    @Order(3)
+    public void testGetStudentById() throws Exception {
+        Student student = new Student(
+                1L,
+                "Simone",
+                "Riso",
+                true);
+        when(studentService.getStudentById(1L)).thenReturn(Optional.of(student));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/students/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Simone"))
+                .andExpect(jsonPath("$.lastName").value("Riso"))
+                .andExpect(jsonPath("$.working").value(true));
+    }
+
+    @Test
+    @Order(4)
+    public void testUpdateStudent() throws Exception {
+        Student updatedStudent = new Student(
+                1L,
+                "Simon",
+                "Rice",
+                true);
+        when(studentService.updateStudent(eq(1L), any(Student.class))).thenReturn(updatedStudent);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/students/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":1,\"name\":\"Simon\",\"lastName\":\"Rice\",\"working\":true}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").value("Simon"))
+                .andExpect(jsonPath("$.lastName").value("Rice"))
+                .andExpect(jsonPath("$.working").value(true));
+    }
+
+    @Test
+    @Order(5)
+    public void testUpdateIsWorking() throws Exception {
+        doNothing().when(studentService).updateIsWorking(eq(1L), anyBoolean());
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/students/1?working=true"))
+                .andExpect(status().isOk());
+
+        verify(studentService, times(1)).updateIsWorking(eq(1L), eq(true));
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteStudent() throws Exception {
+        doNothing().when(studentService).deleteStudent(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/students/1"))
+                .andExpect(status().isNoContent());
+
+        verify(studentService, times(1)).deleteStudent(1L);
+    }
+
 }
--- esercizio_crud_test/src/test/java/co/develhope/esercizio_crud_test/StudentServiceTest.java
+++ esercizio_crud_test/src/test/java/co/develhope/esercizio_crud_test/StudentServiceTest.java
@@ -1,52 +1,72 @@
 package co.develhope.esercizio_crud_test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import co.develhope.esercizio_crud_test.entities.Student;
 import co.develhope.esercizio_crud_test.repo.StudentRepository;
 import co.develhope.esercizio_crud_test.service.StudentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class StudentServiceTest {
 
     @Autowired
-    private StudentService studentService;
+    private StudentRepository studentRepository;
 
     @Autowired
-    private StudentRepository studentRepository;
+    private StudentService studentService;
 
     @Test
     public void testCreateStudent() {
-        Student student = new Student();
-        student.setName("Simon");
-        student.setWorking(true);
-
+        Student student = new Student(1L,"Simone", "Riso", true);
         Student createdStudent = studentService.createStudent(student);
 
-        assertEquals(student, createdStudent);
+        assertEquals(student.getId(), createdStudent.getId());
+        assertEquals(student.getName(), createdStudent.getName());
+        assertEquals(student.getLastName(), createdStudent.getLastName());
+        assertEquals(student.isWorking(), createdStudent.isWorking());
+    }
+
+    @Test
+    public void testUpdateStudent() {
+        Student student = new Student(1L,"Simon", "Rice", true);
+        Student createdStudent = studentService.createStudent(student);
+
+        Student updatedStudent = new Student(createdStudent.getId(), "Nomis", "Ecir", false);
+        Student result = studentService.updateStudent(updatedStudent.getId(), updatedStudent);
+
+        assertEquals(updatedStudent.getId(), result.getId());
+        assertEquals(updatedStudent.getName(), result.getName());
+        assertEquals(updatedStudent.getLastName(), result.getLastName());
+        assertEquals(updatedStudent.isWorking(), result.isWorking());
     }
 
     @Test
     public void testGetAllStudents() {
-        Student student1 = new Student();
-        student1.setName("Emiliano");
-        student1.setWorking(true);
-        studentRepository.save(student1);
+        Student testStudent = new Student(1L, "Simone", "Riso", true);
+        studentRepository.save(testStudent);
 
-        Student student2 = new Student();
-        student2.setName("Gianna");
-        student2.setWorking(false);
-        studentRepository.save(student2);
+        List<Student> students = studentService.getAllStudents();
 
-        List<Student> allStudents = studentService.getAllStudents();
+        assertTrue(!students.isEmpty(), "La lista degli studenti non dovrebbe essere vuota");
+    }
 
-        assertEquals(2, allStudents.size());
 
-        assertEquals("Emiliano", allStudents.get(0).getName());
-        assertEquals("Gianna", allStudents.get(1).getName());
+    @Test
+    public void testDeleteStudent() {
+        Student student = new Student(1L,"Simone", "Riso", true);
+        Student createdStudent = studentService.createStudent(student);
+        studentService.deleteStudent(createdStudent.getId());
+
+        List<Student> remainingStudents = studentService.getAllStudents();
+        assertFalse(remainingStudents.contains(student));
     }
 
 }
